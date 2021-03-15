0d5e81a3335d33243ccc4bb32c4986518f0ca9b0
Anindita Som <aninditasom2020@gmail.com>
2021-03-15 15:18:47 +0000

    initial commit django module

---
+++ todo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .models import items
+
+
+# Create your views here.
+
+
+def get_todo_list(request):
+    # return HttpResponse('<h1>Hello</h1><p>This is a paragraph</p>')
+    items1 = items.objects.all()
+    context = {
+        'items': items1
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        items.objects.create(name=name, done=done)
+        return redirect('get_todo_list')
+
+    return render(request, 'todo/add_item.html')
