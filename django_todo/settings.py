ad1f0984abe604784a876c94dc3244a7932b8ac1
Anindita Som <aninditasom2020@gmail.com>
2021-03-22 15:21:19 +0000

    adding secret key to config vars

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'sphi%2wf4b@6$+n0k&b(g!i75ow1olrj$5hb3c25ppbh46-$gt')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
